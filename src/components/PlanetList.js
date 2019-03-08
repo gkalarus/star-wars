215fba663e785065b29bd4055f23ed77c2a57a23
Grzegorz <kalarus.g@gmail.com>
2019-03-08 15:38:56 +0100

    Add movie - planet styling

--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PlanetList = ({planets, handlePlanetSelection}) => {
   let planetList = planets.map(planet => <li onClick={() => handlePlanetSelection(planet.name)} key={planet.name}>{planet.name}</li>)
   return (  
-    <ul>
+    <ul className="planetList">
       {planetList}
     </ul>
   );
--- src/components/SelectedPlanets.js
+++ src/components/SelectedPlanets.js
@@ -6,7 +6,7 @@ const SelectedPlanets = ({selectedPlanets, handleDeletePlanet}) => {
   let lis = selectedPlanets.map(planet => <li key={planet}>{planet} <button onClick={(e) => handleDeletePlanet(e, planet)}><img src={deletePlanet} alt="delete planet button"/></button></li>)
 
   return (  
-    <ul>{lis}</ul>
+    <ul className="selectedPlanets">{lis}</ul>
   );
 }
  
